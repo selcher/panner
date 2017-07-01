08964be78dd5ef27878c18e15ddf4d79d46ddc85
Selcher <selcher123@gmail.com>
2017-07-02 00:30:48 +0800

    Replace tabs with space

--- test/index.js
+++ test/index.js
@@ -1,22 +1,22 @@
 'use strict';
 
 var should = require('chai').should(),
-	panner = require('../src/index');
+    panner = require('../src/index');
 
 describe('init', () => {
-	it('adds mousemove event listener on passed element', () => {
-		var evtName = null;
-		var evtMethod = null;
-		var elem = {
-			addEventListener: (eventName, callback) => {
-				evtName = eventName;
-				evtMethod = callback;
-			}
-		};
+    it('adds mousemove event listener on passed element', () => {
+        var evtName = null;
+        var evtMethod = null;
+        var elem = {
+            addEventListener: (eventName, callback) => {
+                evtName = eventName;
+                evtMethod = callback;
+            }
+        };
 
-		elem = panner.init(elem);
+        elem = panner.init(elem);
 
-		(evtName).should.equal('mousemove');
-		(typeof evtMethod).should.equal('function');
-	});
+        (evtName).should.equal('mousemove');
+        (typeof evtMethod).should.equal('function');
+    });
 });
